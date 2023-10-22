e0d42b30e8947ed31238be622a345ecb546db225
riddhish <riddhish.k@somaiya.edu>
2023-10-22 15:41:15 +0530

    RequestHistoryDone

--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,76 @@
-import './index.css';
+import { useEffect, useState } from "react";
+import "./index.css";
 
-function Home(){
-    return(
-        <>
-        <h1 className="home" >Welcome to the home page</h1>
-        </>
-    )
+function Home() {
+  const [requestArray, SetRequestArray] = useState([]);
+
+  const [name, setName] = useState();
+  const [email, setEmail] = useState();
+
+  const nameChanged = (event) => {
+    setName(event.target.value);
+  };
+
+  const emailChanged = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const SubmitEvent = (event) => {
+    event.preventDefault();
+    SetRequestArray((past) => [...past, { name: name, email: email }]);
+  };
+
+  useEffect(()=>{
+
+    setTimeout(()=>{
+        alert("Hi there");
+    },5000);
+
+  },[requestArray]);
+
+  return (
+    <>
+      <div className="home">
+        <form>
+          <label htmlFor="nameInput">First name:</label>
+          <br></br>
+          <input
+            type="text"
+            name="name"
+            id="nameInput"
+            placeholder="Enter Your Name"
+            onChange={nameChanged}
+          ></input>
+          <br></br>
+          <label htmlFor="email">Email:</label>
+          <br></br>
+          <input
+            type="text"
+            name="email"
+            id="email"
+            placeholder="Enter your Email"
+            onChange={emailChanged}
+          ></input>
+          <br></br>
+          <input
+            type="submit"
+            value="Get All products List"
+            onClick={SubmitEvent}
+          ></input>
+        </form>
+      </div>
+      <div>
+        {requestArray.length == 0 ? "" : <p>History Of Requests</p>}
+        {requestArray.length == 0
+          ? ""
+          : requestArray.map((request, key) => (
+              <p key={key}>
+                Name :{request.name} and Email: {request.email}
+              </p>
+            ))}
+      </div>
+    </>
+  );
 }
 
 export default Home;
--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import Home from './Home';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
+    <h1 className='welcome'>Hello, Welcome to TechnoPharma</h1>
     <Home/>
   </React.StrictMode>
 );
